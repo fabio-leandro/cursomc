9739b7f0815b19c965f1660a20670a34135dffc9
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-03-14 15:55:58 -0300

    Test Rest

--- src/main/java/com/fabio/cursomc/resources/CategoriesResource.java
+++ src/main/java/com/fabio/cursomc/resources/CategoriesResource.java
@@ -1,2 +1,16 @@
-package com.fabio.cursomc.resources;public class CategoriaResource {
+package com.fabio.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoriesResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getCategories(){
+        return "Find categories";
+    }
+
 }
