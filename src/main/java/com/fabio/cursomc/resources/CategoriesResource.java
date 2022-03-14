ab9fb63fc7ed335450c39c57d32b217e37277b4e
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-03-14 16:20:58 -0300

    Criando categoria

--- src/main/java/com/fabio/cursomc/resources/CategoriesResource.java
+++ src/main/java/com/fabio/cursomc/resources/CategoriesResource.java
@@ -1,16 +1,25 @@
 package com.fabio.cursomc.resources;
 
+import com.fabio.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriesResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String getCategories(){
-        return "Find categories";
+    public List<Category> getCategories(){
+        Category cat1 = new Category(1, "Informatica");
+        Category cat2 = new Category(2, "Office");
+        List<Category> categories = new ArrayList<>();
+        categories.add(cat1);
+        categories.add(cat2);
+        return categories;
     }
 
 }
