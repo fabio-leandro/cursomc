ea03b76889c22803473f55d60add1d1147563295
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-03-17 16:01:12 -0300

    Criação do objeto Produto e Associação muitos para muitos

--- src/main/java/com/fabio/cursomc/CursomcApplication.java
+++ src/main/java/com/fabio/cursomc/CursomcApplication.java
@@ -1,19 +1,25 @@
 package com.fabio.cursomc;
 
 import com.fabio.cursomc.domain.Category;
+import com.fabio.cursomc.domain.Product;
 import com.fabio.cursomc.repositories.CategoryRepository;
+import com.fabio.cursomc.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Category c1 = new Category(null,"Informatica");
 		Category c2 = new Category(null, "Office");
 
+		Product p1 = new Product(null, "Computer",2000.00);
+		Product p2 = new Product(null,"Printer",800.00);
+		Product p3 = new Product(null,"Mouse",80.00);
+
+		c1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProducts().addAll(List.of(p2));
+
+		p1.getCategories().addAll(List.of(c1));
+		p2.getCategories().addAll(Arrays.asList(c1,c2));
+		p3.getCategories().addAll(List.of(c1));
+
 		categoryRepository.saveAll(Arrays.asList(c1,c2));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 	}
 }
